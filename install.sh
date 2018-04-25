17fc3e76767541179329c4d52f5b0cb0e6d56ef8
Diego Varela <dvsilva@veltec.com.br>
2018-04-25 08:20:56 -0300

    Add more veltec config

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ echo "Instalando Google Chrome"
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo rm /etc/apt/sources.list.d/google.list
-sudo apt-get update
+sudo apt update
 sudo apt install google-chrome-stable -y
 
 echo "Instalando Git"
