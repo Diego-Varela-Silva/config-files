619d469c6458150f889a06fe7d11f0b6c4d702f9
Diego Varela <dvsilva@veltec.com.br>
2019-01-14 08:52:46 -0200

    Update config

--- alias.sh
+++ alias.sh
@@ -1,15 +1,29 @@
+ZSH_THEME="robbyrussell"
 
+COMPLETION_WAITING_DOTS="true"
 
-#Personal alias for multiple purposes
+plugins=(git mvn npm adb)
 
-# plugins git mvn adb
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='nano'
+else
+  export EDITOR='nano'
+fi
 
-color_log() {
+colorLog() {
+    # $1 log
+    # $2 pattern (opcional)
     clear
-    if [ -z ${2} ]; then cat $1 | ccze -A; else cat $1 | ccze -A | grep $2; fi
+    if [ -z ${2} ]; then cat $1 | ccze -A; else cat $1 | ccze -A | grep -i $2; fi
 }
 
-get_database() {
+findInLogs() {
+    # $1 log
+    # $2 pattern
+    grep -rnw $1 -e $2
+}
+
+getDatabase() {
     echo "adb -d shell \"run-as $1 rm /sdcard/$2\""
     echo "adb -d shell \"run-as $1 cp /data/data/$1/databases/$2 /sdcard/$2\""
     echo "adb pull /sdcard/$2"
@@ -19,15 +33,22 @@ alias update="sudo apt update && sudo apt dist-upgrade"
 alias clean="sudo apt autoremove && sudo apt autoclean"
 alias alias_edit="code ~/.zshrc"
 alias profile_edit="code ~/.profile"
+alias i3_edit="code ~/.config/i3/config"
 alias gh="gitk &"
-alias gssu="git stash save --include-untracked"
+alias gstsu="git stash save --include-untracked"
 alias adbi="adb install platforms/android/build/outputs/apk/debug/android-debug.apk"
+alias adbi2="adb install platforms/android/app/build/outputs/apk/debug/app-debug.apk"
 alias adbu="adb uninstall"
 alias clear_data="adb shell pm clear"
-alias is="ionic serve --no-open"
+alias is="ionic serve --no-open -c"
+alias icral="ionic cordova run android -lcs --livereload-port 8101"
 alias icra="ionic cordova run android"
-alias icral="ionic cordova run android -lsc --livereload-port 8101"
 alias ira="ionic run android"
 alias icea="ionic cordova emulate android"
 alias icba="ionic cordova build android"
 alias sav='env WINEPREFIX="/home/dvsilva/.wine" wine C:\\windows\\command\\start.exe /Unix /home/dvsilva/.wine/dosdevices/c:/ProgramData/Microsoft/Windows/Start\ Menu/Programs/Sav/Sav.lnk'
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:/usr/local/go/bin:$HOME/go/bin"
